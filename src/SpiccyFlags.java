45b6adae6fbe73d4d4a2cd7df95d8c0b90f35ac2
Francisco67 <francisco.sampedro56@gmail.com>
2018-12-03 12:59:59 -0500

    Unit Start
    added:
    -static void main() to main class
    - added initiation to abstract unit()
    - added paint to abstract unit()

--- src/SpiccyFlags.java
+++ src/SpiccyFlags.java
@@ -1,4 +1,22 @@
+import javax.swing.*;
+
 public class SpiccyFlags {
     private int blueBoon, redBoon;
     private boolean turnOrder;
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        JFrame f = new JFrame("Sorting Room");
+        SpiccyFlags c = new SpiccyFlags();
+        f.setSize(1020,640);
+        f.setVisible(true);
+        f.add(c);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while (true)
+        {
+
+            Thread.sleep(10);
+        }
+    }
 }
--- src/Unit.java
+++ src/Unit.java
@@ -1,5 +1,24 @@
+import java.awt.*;
+
 public abstract class Unit {
-    private int health, rangeAttack, rangeMomement, attackValuw, unitType;
+    private int health, rangeAttack, rangeMovement, attackValue, unitType, x, y;
     private boolean isUpgraded, side;
-    FOOK
+
+    public Unit(int h, int ra, int rm, int a, int u, int x, int y, boolean upgraded, boolean side)
+    {
+        health=h;
+        rangeAttack=ra;
+        rangeMovement=rm;
+        attackValue=a;
+        unitType=u;
+        this.x=x;
+        this.y=y;
+        isUpgraded=upgraded;
+        this.side=side;
+
+    }
+
+    public void paint(Graphics2D g) {
+
+    }
 }
