661f8f0fe3e93ae66932d3276970a478af08ab59
Mana234 <manaman234@gmail.com>
2018-12-04 13:04:39 -0500

    Added mouseEvents to units

--- src/Unit.java
+++ src/Unit.java
@@ -1,8 +1,9 @@
 import java.awt.*;
+import java.awt.event.*;
 
-public abstract class Unit {
+public abstract class Unit implements MouseListener {
     private int health, rangeAttack, rangeMovement, attackValue, unitType, x, y;
-    private boolean isUpgraded, side;
+    private boolean isUpgraded, side, isClicked;
 
     public Unit(int h, int ra, int rm, int a, int u, int x, int y, boolean upgraded, boolean side)
     {
@@ -17,8 +18,20 @@ public abstract class Unit {
         this.side=side;
 
     }
-
+    public void mousePressed(MouseEvent e) {
+        if (e.getX() > x && e.getX() < x+20 && e.getY() > y && e.getY() < y+20){
+            isClicked = true;
+        }
+    }
+    public void mouseReleased(MouseEvent e){
+        if (isClicked = true){
+            x = e.getX();
+            y = e.getY();
+            isClicked = false;
+        }
+    }
     public void paint(Graphics2D g) {
+
 
     }
 }
