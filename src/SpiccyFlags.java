3f574b8fe47384b1aa2595dff2fef65ff5e70120
Francisco67 <francisco.sampedro56@gmail.com>
2019-01-09 13:37:01 -0500

    added notes

--- src/SpiccyFlags.java
+++ src/SpiccyFlags.java
@@ -13,16 +13,19 @@ public class SpiccyFlags extends JPanel{
     private Unit[] red = new Unit[5];
 
     public SpiccyFlags() {
+        //number of boons each side has
         blueBoon = 2;
         redBoon = 2;
+        //create Blue Units
         blue[3] = new Mage(100, 100, 500, 100, 100, 100, false, true);
         blue[1] = new Infantry(100, 100, 500, 50, 100, 150, false, true);
-        blue[2] = new Armor(100, 100, 500, 100, 100, 200, false, true);
+        blue[2] = new Armour(100, 100, 500, 100, 100, 200, false, true);
         blue[0] = new Cavalier(100, 100, 500, 100, 100, 250, false, true);
         blue[4] = new Ranger(100, 100, 500, 100, 100, 300, false, true);
+        //create Red units
         red[3] = new Mage(100, 100, 500, 100,  500, 100, false, false);
         red[1] = new Infantry(100, 100, 500, 50, 500, 150, false, false);
-        red[2] = new Armor(100, 100, 500, 100, 500, 200, false, false);
+        red[2] = new Armour(100, 100, 500, 100, 500, 200, false, false);
         red[0] = new Cavalier(100, 100, 500, 100, 500, 250, false, false);
         red[4] = new Ranger(100, 100, 500, 100,  500, 300, false, false);
 
@@ -30,15 +33,16 @@ public class SpiccyFlags extends JPanel{
             public void mouseExited(MouseEvent e){}
             public void mouseClicked(MouseEvent e){}
             public void mouseEntered(MouseEvent e){}
+            //check if unit is clicked
             public void mousePressed(MouseEvent e){
                 for (int i = 0; i<5; i++){
                     blue[i].mousePressed(e.getX(), e.getY());
                     red[i].mousePressed(e.getX(), e.getY());
                 }
-
-
             }
+            //runs what happens when a unit is released
             public void mouseReleased(MouseEvent e) {
+                //runs activated abilities
                 for (int i = 3; i<=4; i++){
                     for(int t = 0; t<5; t++)
                     {
@@ -46,27 +50,29 @@ public class SpiccyFlags extends JPanel{
                         red[i].ability(e.getX(),e.getY(),blue[t]);
                     }
                 }
-                for(int retry=0;retry<2;retry++) {
-                    for (int t = 0; t < 5; t++) {
-                        for (int i = 0; i < 5; i++) {
-                            blue[i].mouseReleased(e.getX(), e.getY());
-                            blue[i].combat(red[t]);
+                //moves units and runs combat
+                for (int t = 0; t < 5; t++) {
+                    for (int i = 0; i < 5; i++) {
+                        blue[i].mouseReleased(e.getX(), e.getY());
+                        red[i].mouseReleased(e.getX(), e.getY());
 
-                            red[i].mouseReleased(e.getX(), e.getY());
-                            red[i].combat(blue[t]);
-                        }
+                        blue[i].combat(red[t]);
+                        red[i].combat(blue[t]);
                     }
-                    for (int t = 0; t < 5; t++) {
-                        if (blue[t].getIsDead())
-                            blue[t].setX(-100);
-                        if (red[t].getIsDead())
-                            red[t].setX(-100);
-                    }
+                }
+                //moves dead units offscreen
+                for (int t = 0; t < 5; t++) {
+                    if (blue[t].getIsDead())
+                        blue[t].setX(-100);
+                    if (red[t].getIsDead())
+                        red[t].setX(-100);
                 }
 
             }
         });
+
         addMouseMotionListener(new MouseMotionListener() {
+            //makes units move with mouse when clicked
             public void mouseDragged(MouseEvent e) {
                 for (int i = 0; i<blue.length; i++){blue[i].mouseDragged(e.getX(), e.getY());}
                 for (int i = 0; i<red.length; i++){red[i].mouseDragged(e.getX(), e.getY());}
@@ -76,10 +82,12 @@ public class SpiccyFlags extends JPanel{
 
             }
         });
+
         addKeyListener(new KeyListener() {
             public void keyTyped(KeyEvent e) {
             }
             @Override
+            //used for any toggling(danger area etc.)
             public void keyReleased(KeyEvent e) {
                 for (int i = 0; i<5; i++){
                     blue[i].keyReleased(e);
@@ -92,35 +100,47 @@ public class SpiccyFlags extends JPanel{
         setFocusable(true);
     }
 
+    //changes turns when all units acts
     public void changeTurn()
     {
+        //counter for units that have acted
         int unitsActed=0;
+
         for(int t=0;t<5;t++)
         {
+            //makes sure dead units are considered to have already acted
             if(blue[t].getIsDead()) {
                 blue[t].setCanAct(false);
             }
             if(red[t].getIsDead()) {
                 red[t].setCanAct(false);
             }
-            if(!blue[t].getAct())
-                unitsActed++;
-            if(!red[t].getAct())
-                unitsActed++;
-            if(unitsActed==10){
 
+            //counts the number of units that have acted
+            if(!blue[t].getAct() ||!red[t].getAct()) {
+                unitsActed++;
+            }
+
+            //if all units have acted, changes turns
+            if(unitsActed==10){
+                //if it is blue's turn
                 if(Turn)
                 {
+                    //lets red units act
                     for(int i=0; i<5; i++) {
                         red[i].setCanAct(true);
                     }
+                    //it is now red side's turn
                     Turn=false;
                 }
+                //if it is red's turn
                 else
                 {
+                    //lets blue units act
                     for(int i= 0; i<5; i++) {
                         blue[i].setCanAct(true);
                     }
+                    //it is now blue side's turn
                     Turn=true;
                 }
             }
@@ -129,30 +149,27 @@ public class SpiccyFlags extends JPanel{
 
     public void paint(Graphics g)
     {
+        //required shtuff
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        //paint background
         g2d.setColor(Color.green);
         g2d.fillRect(0,0,1020,640);
-        g2d.setColor(Color.blue);
+
         for(int t=0; t<5; t++) {
+            //paints danger area if toggled
             if (Turn)
                 red[t].showDanger(g2d);
             else
                 blue[t].showDanger(g2d);
-        }
 
-        for(int t=0; t<5; t++)
-        {
-            if(!blue[t].getIsDead()) {
+            //paints Units
                 blue[t].paint(g2d);
-            }
-            if(!red[t].getIsDead()) {
                 red[t].paint(g2d);
-            }
-
         }
     }
 
+    //what can i say it's the main class
     public static void main(String[] args) throws InterruptedException
     {
         JFrame f = new JFrame("spiccyFlags");
--- src/Unit.java
+++ src/Unit.java
@@ -4,66 +4,81 @@ import java.awt.image.BufferedImage;
 
 
 public abstract class Unit{
-    //Blue team is the true team!
+    //Blue team is the true team!(blue.side=true, red.side=false)
     private int health,maxHealth, rangeAttack, rangeMovement, attackValue,x, y, mx, my;
-    private boolean isUpgraded, side, isClicked, canAct, isDead, toggle, highlight;
-    BufferedImage red, redUsed, redUp, redUpUsed, blue, blueUsed, blueUp, blueUpUsed;
+    private boolean isUpgraded, side, isClicked, canAct, isDead, toggle, highlight,isGuarded;
+    private BufferedImage red, redUsed, redUp, redUpUsed, blue, blueUsed, blueUp, blueUpUsed;
 
     public Unit(int h, int ra, int rm, int a, int x, int y, boolean upgraded, boolean side)
     {
+        //set stats
         health=h;
         maxHealth=h;
         rangeAttack=ra;
         rangeMovement=rm;
         attackValue=a;
+        //set location
         this.x=x;
         this.y=y;
         mx=x;
         my=y;
+        //set booleans
         isUpgraded=upgraded;
         this.side=side;
         canAct=side;
         isDead=false;
+        isGuarded=false;
 
+        //create location to store blue images
         blue=null;
         blueUp=null;
         blueUpUsed=null;
         blueUsed=null;
-
+        //create location to store red images
         red=null;
         redUp=null;
         redUpUsed=null;
         redUsed=null;
     }
 
+    //location getters
     public int getX(){return x;}
     public int getY(){return y;}
     public int getMX() {return mx;}
     public int getMY() {return my;}
+    //stats getters
     public int getRM(){return rangeMovement;}
     public int getRA(){return rangeAttack;}
     public int getAV(){return attackValue;}
     public int getH(){return health;}
-    public int getMH() {return maxHealth; }
+    public int getMH() {return maxHealth;}
+    //boolean getters
     public boolean getAct(){return canAct;}
     public boolean getIsDead(){return isDead;}
     public boolean isClicked(){return isClicked;}
-
+    //setters
     public void setH(int h){health = h;}
     public void setX(int X){x=X;}
     public void setY(int Y){y=Y;}
     public void setCanAct(boolean act){canAct=act;}
     public void setDead(boolean dead){isDead=dead;}
+
     public void mousePressed(int X, int Y){
+        //checks if turn unit is clicked & stores unit's original position(mx & my)
         if (X > x && X < x+20 && Y > y && Y < y+20 && canAct)
             isClicked = true; mx = x; my = y;
 
+        //toggle off highlight
         if(X > x && X < x+20 && Y > y && Y < y+20 && !canAct&& highlight || canAct)
             highlight=false;
+
+        //toggle on highlight
         else if(X > x && X < x+20 && Y > y && Y < y+20 )
             highlight=true;
 
     }
+
+    //moves clicked unit along with mouse
     public void mouseDragged(int X, int Y){
         if(isClicked)
         {
@@ -71,20 +86,31 @@ public abstract class Unit{
             y=Y-10;
         }
     }
+
     public void mouseReleased(int X, int Y) {
+
+        //checks if released in the area it can move
         if (isClicked && !isDead && Math.sqrt((Math.pow((X - mx - 10), 2)) + Math.pow((Y - my - 10), 2)) < rangeMovement / 2) {
+            //moves clicked unit to where the mouse is released
             x = X - 10;
             y = Y - 10;
+            //unit has acted
             canAct = false;
         }
+
+        //checks if released in an area the unit CAN'T move to
         else if(Math.sqrt((Math.pow((X - mx - 10), 2)) + Math.pow((Y - my - 10), 2)) > rangeMovement / 2){
+            //returns to original position
             x=mx;
             y=my;
         }
+
+        //unit is no longer clicked
         isClicked = false;
     }
 
     public void keyReleased(KeyEvent e) {
+        //toggles showDangerArea with spacebar click
         if (e.getKeyCode() == KeyEvent.VK_SPACE&&toggle)
         {
             toggle=false;
@@ -95,17 +121,20 @@ public abstract class Unit{
         }
     }
 
-    public void ability(int X, int Y, Unit other)
-    {
+    //just a generic class for subclass' triggered abilities
+    public void ability(int X, int Y, Unit other) {}
 
-    }
-
+    //runs combat
     public void combat(Unit other){
+        //checks if opponent is close to unit
         if (Math.sqrt((Math.pow((other.x-x-10), 2)) + Math.pow((other.y - y - 10), 2)) < rangeAttack/2 ) {
+            //damages opponent equal to attack value
             other.setH(other.getH() - attackValue);
+            //checks if other unit has health, sets unit dead if not
             if (other.getH() < 0)
                 other.isDead = true;
         }
+        //checks
         if (Math.sqrt((Math.pow((other.x - x - 10), 2)) + Math.pow((other.y - y - 10), 2)) < other.rangeAttack/2 ) {
             health = health - other.attackValue;
             if (health < 0)
