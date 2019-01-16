4b7321a3f56aec6897d0ab931bcca4eae4da841e
Francisco67 <francisco.sampedro56@gmail.com>
2019-01-16 13:17:51 -0500

    WE'RE HALF WAY THEEEEERE, WOOOOOOOOAAAH! SQUIDWARD ON A CHAIR!

--- src/Armor.java
+++ src/Armor.java
@@ -2,8 +2,8 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
-public class Armour extends Unit{
-    public Armour(int h, int ra, int rm, int a, int x, int y, boolean upgraded, boolean side){
+public class Armor extends Unit{
+    public Armor(int h, int ra, int rm, int a, int x, int y, boolean upgraded, boolean side){
         super(h,ra,rm,a,x,y,upgraded,side);
         try {
             blue = ImageIO.read(new File("res\\armourBlue.png"));
